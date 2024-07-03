94dedeb6ca6fb2756ba80406aebc38e2821bee4c
user2oo4 <hiepphamm04@gmail.com>
2024-07-02 22:17:45 -0400

    idk

---
+++ banking/data.md
@@ -0,0 +1,96 @@
+# Tính năng nổi bật
+
+
+
+# Loại giao dịch
+
+
+
+# Trạng thái thẻ
+
+
+
+# Hình thức thẻ
+
+
+
+# Phí
+
+
+
+# Thông tin chung về thẻ
+
+
+
+# Chi tiết giao dịch
+
+
+
+# Các loại khách hàng
+
+
+
+# Lợi ích KH
+
+
+
+# Mã sản phẩm
+
+
+
+# Đặc điểm chi tiết thẻ
+
+
+
+# Loại thẻ
+
+
+
+# Tên thẻ
+
+
+
+# Loại hạn mức
+
+
+
+# Hạn mức số lần giao dịch
+
+
+
+# Tên thẻ - Tên thẻ thay thế
+
+
+
+# Mã SP - Mã SP tại Corebanking mới
+
+
+
+# Tính năng nổi bật - Thẻ
+
+
+
+# Thông tin chung về thẻ - Loại thẻ
+
+
+
+# Loại thẻ - Lợi ích KH
+
+
+
+# Chi tiết các thẻ
+
+
+
+# Loại GD - Loại thẻ - Chi tiết GD
+
+
+
+# Tên thẻ - Loại giao dịch - Hạn mức
+
+
+
+# Tên thẻ - Loại giao dịch - Số lần GD
+
+
+
--- codes/excel_read.py
+++ codes/excel_read.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-file_name = 'banking'
+file_name = 'aimentor/'
 # Specify the path to your Excel file
-file_path = file_name + '.xlsx'
+file_path = file_name + 'data.xlsx'
 
 # Read all sheets into a dictionary of DataFrames
 dfs = pd.read_excel(file_path, sheet_name=None)
 
 # Specify the output markdown file path
 md_file_path = file_name + '.md'
-txt_file_path = file_name + '.txt'
+txt_file_path = file_name + 'data.txt'
+vertices_file_path = file_name + 'knowledge_vertices.py'
+graph_file_path = file_name + 'knowledge_graph.py'
 vertices = dict()
 edges = dict()
 
@@ -38,13 +40,13 @@ with open(md_file_path, 'w', encoding='utf-8') as f:
             for col in cols:
                 ncol = check_and_remove_number(col)
                 if ncol not in vertices:
-                    vertices[ncol] = []
+                    vertices[ncol] = [] 
                 if ncol in new_cols:
                     df = df.drop(columns=col)
                 else:
                     df = df.rename(columns={col:ncol})
                     new_cols.append(ncol)
-            #print(df)
+            print(new_cols)
             new_cols = tuple(new_cols)
             edges[new_cols] = []
             rows = df.iterrows()
@@ -56,20 +58,24 @@ with open(md_file_path, 'w', encoding='utf-8') as f:
                         vertices[col].append(row[col])
         f.write('\n\n')
 
-with open(txt_file_path, 'w', encoding='utf-8') as f:
+with open(vertices_file_path, 'w', encoding='utf-8') as f:
+    f.write('knowledge_vertices = {')
     for key, value in vertices.items():
         f.write(f"\t\'{key}\': [\n")
         for item in value:
             f.write(f"\t\t\'{item}\',\n")
         f.write(f"\t],\n")
+    f.write('}')
 
 
 knowledge_graph = dict()
 
 for key in edges.keys():
     # key is a tuple
+    print(key)
     for id in range(0, len(key)):
         new_tuple = key[id: id + 1] + key[:id] + key[id + 1:]
+        #print(new_tuple)
         knowledge_graph[new_tuple] = dict()
         for value in edges[key]:
             # value is a tuple as wellf
@@ -78,12 +84,15 @@ for key in edges.keys():
             new_value_tuple = value[:id] + value[id + 1:]
             knowledge_graph[new_tuple][value[id]] = new_value_tuple
 
-with open(txt_file_path, 'a', encoding='utf-8') as f:
+
+with open(graph_file_path, 'w', encoding='utf-8') as f:
+    f.write('knowledge_graph = {\n')
     for key, value in knowledge_graph.items():
-        f.write(f"\t\'{key}\': [\n")
-        print(type(value))
+        f.write(f"\t{key}: ")
+        f.write("{\n")
         for inner_key, inner_value in value.items():
             f.write(f"\t\t\'{inner_key}\': {inner_value},\n")
-        f.write(f"\t],\n")
+        f.write("\t},\n")
+    f.write('}')
 
 #print(f"Markdown tables saved to {md_file_path}")
